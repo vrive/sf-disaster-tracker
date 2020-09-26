63d345018c15e6e836ca5ce6567331e4f096a845
vrive <vrive072@fiu.edu>
2020-09-26 05:30:16 -0400

    Add firebase for addingg/retrieving incidents and resources

---
+++ models/Incident.js
@@ -0,0 +1,11 @@
+class Incident{
+    constructor(type, location, photo, notes, county){
+        this.type = type;
+        this.location = location;
+        this.photo = photo;
+        this.notes = notes;
+        this.county = county;
+    }
+}
+
+export default Incident;
---
+++ models/Resource.js
@@ -0,0 +1,11 @@
+class Resource{
+    constructor(type, location, photo, notes,county){
+        this.type = type;
+        this.location = location;
+        this.photo = photo;
+        this.notes = notes;
+        this.county = county;
+    }
+}
+
+export default Resource;
