96471b268989b1f4dcd236610ae08b90ec04d293
vrive <vrive072@fiu.edu>
2020-09-26 01:16:28 -0400

    added templates for screens

---
+++ screens/CountiesScreen.js
@@ -0,0 +1,17 @@
+import React from 'react';
+import {View, StyleSheet} from 'react-native';
+
+const CountiesScreen = props => {
+    return (
+        <View style={styles.container}></View>
+    );
+};
+
+const styles = StyleSheet.create({
+    container:{
+
+    }
+});
+
+export default CountiesScreen;
+
---
+++ screens/MapScreen.js
@@ -0,0 +1,16 @@
+import React from 'react';
+import {View, StyleSheet} from 'react-native';
+
+const MapScreen = props => {
+    return (
+        <View style={styles.container}></View>
+    );
+};
+
+const styles = StyleSheet.create({
+    container:{
+
+    }
+});
+
+export default MapScreen;
