5fe110d4e04f1cc79961cde576595773c573f89d
vrivero09 <rivero0288@outlook.com>
2020-09-26 15:18:53 -0400

    added map component

--- screens/MapScreen.js
+++ screens/MapScreen.js
@@ -1,14 +1,31 @@
 import React from 'react';
-import {View, StyleSheet, Text} from 'react-native';
+import {StyleSheet, View, Text} from 'react-native';
 import MapView from 'react-native-maps';
 
 class MapScreen extends React.Component {
 
+
+    
     render(){
         return (
-        <View style={styles.container}> 
-            <Text>Maps pageeee!</Text>
-        </View>
+            <View style={styles.container}>
+                <MapView style={styles.map}
+                    initialRegion={{
+                    latitude: 37.78825,
+                    longitude: -122.4324,
+                    latitudeDelta: 0.0,
+                    longitudeDelta: 0.0,
+                    }}
+        >
+                <MapView.Marker
+                coordinate={{latitude: 25.7712,
+                longitude: -80.1895}}
+                title={"title"}
+                description={"description"}
+            />
+        </MapView>
+
+            </View>
         );
 
     }
@@ -28,7 +45,14 @@ const styles = StyleSheet.create({
       fontSize: 30,
       alignItems: 'center',
       justifyContent: 'center',
-    }
+    },
+    map: {
+        position: 'absolute',
+        top: 0,
+        left: 0,
+        right: 0,
+        bottom: 0,
+      }
   
   });
 
