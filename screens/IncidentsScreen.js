79b35d2acd5817b513a130d15577bfc532d028d0
vrivero09 <rivero0288@outlook.com>
2020-09-26 09:22:29 -0400

    missed this part

--- screens/IncidentsScreen.js
+++ screens/IncidentsScreen.js
@@ -1,16 +1,25 @@
 import React from 'react';
-import {View, StyleSheet} from 'react-native';
+import {View, StyleSheet, Text, Button} from 'react-native';
 
-const IncidentsScreen = props => {
-    return (
-        <View style={styles.container}></View>
-    );
-};
+class IncidentsScreen extends React.Component {
+    render(){
+        return (
+            <View style={styles.container}>
+                <Text>Incident Page</Text>    
+            </View>
+        );
+    };
+    }
+
 
 const styles = StyleSheet.create({
     container:{
-
+        flex: 1,
+        backgroundColor: '#ACCCBC',
+        alignItems: 'center',
+        justifyContent: 'center',
     }
 });
+
 
 export default IncidentsScreen;
