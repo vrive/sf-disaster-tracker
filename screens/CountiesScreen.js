afde0793119a519c4fc555db77fcbbf028681131
vrive <vrive072@fiu.edu>
2020-09-27 04:28:58 -0400

    Added Steven's part

--- screens/CountiesScreen.js
+++ screens/CountiesScreen.js
@@ -1,31 +1,58 @@
 import React from 'react';
-import {View, StyleSheet, Text, Button} from 'react-native';
+import { StyleSheet, Text, View, FlatList, Dimensions, Button,TouchableOpacity } from 'react-native';
+
+const data = [
+    { key: 'Miami-Dade' }, { key: 'Palm Beach' }, { key: 'Broward' }, { key: 'St. Lucie' }, { key: 'Monroe' }, { key: 'Martin' }, { key: 'Indian River' }
+];
+
+const numColumns = 3;
 
 class CountiesScreen extends React.Component {
-    render(){
+    renderItem = ({ item, index }) => {
         return (
-            <View style={styles.container}>
-                <Text>List of Counties goes here</Text>
-                <Text>Miami-Dade</Text>
-                <Text>Miami-Dade</Text>
-                <Text>Miami-Dade</Text>
-                <Text>etc</Text>
-                <Button title="Go to Report page"
-                        onPress={() => this.props.navigation.navigate('Report')}/>
+            <View
+                style={styles.item}
+            >
+                <TouchableOpacity onPress={() => this.props.navigation.navigate('Report')}>
+                <Text style={styles.text} > {item.key}</Text>
+                </TouchableOpacity>
+                
             </View>
         );
     };
+
+    render() {
+        return (
+            <View>
+                <FlatList
+                    data={data}
+                    //style={styles.container}
+                    renderItem={this.renderItem}
+                    numColumns={numColumns}
+                />
+            </View>
+        );
     }
+}
 
 
 const styles = StyleSheet.create({
-    container:{
-        flex: 1,
-        backgroundColor: '#ACCCBC',
-        alignItems: 'center',
-        justifyContent: 'center',
-    }
-});
+    container: {
+      flex: 1,
+      marginVertical: 20,
+    },
+    item: {
+      backgroundColor: '#43a359',
+      alignItems: 'center',
+      justifyContent: 'center',
+      flex: 1,
+      margin: 1,
+      height: Dimensions.get('window').width / numColumns, // approximate a square
+    },
+    text: {
+      color: '#fff',
+    },
+  });
 
 export default CountiesScreen;
 
